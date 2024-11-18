4972091184ddb5017864979040a46dc8bdebcf53
Blake <itsmazzyy@gmail.com>
2024-11-18 03:54:08 +0000

    another script update

--- .local/bin/start-replay.sh
+++ .local/bin/start-replay.sh
@@ -3,4 +3,4 @@
 video_path="$HOME/Videos/Clips"
 mkdir -p "$video_path"
 notify-send -t 500 -u low -- "GPU Screen Recorder" "Started recording"
-gpu-screen-recorder -w $(xrandr | grep -o "DP-[0-9]") -f 60 -keyint 1.0 -a "sound/default_output|mic/default_input" -c mp4 -bm cbr -q 26000 -k h264 -r 30 -o "$video_path"
+gpu-screen-recorder -w $(xrandr | grep -o "DP-[0-9]") -f 60 -keyint 1.0 -a "sound/default_output|mic/default_input" -c mp4 -bm cbr -cr full -q 26000 -k h264 -r 30 -o "$video_path"
