0d58268bfc1c55cf9efa444ca3e562c4453f0695
Blake <itsmazzyy@gmail.com>
2025-02-08 17:19:40 +0000

    update for better display recognition

--- .local/bin/start-record.sh
+++ .local/bin/start-record.sh
@@ -5,4 +5,4 @@ video_path="$HOME/Videos/Recordings/"
 echo $timestamp
 mkdir -p "$video_path"
 notify-send -t 500 -u low -- "GPU Screen Recorder" "Started recording"
-gpu-screen-recorder -w $(xrandr | grep -o "DP-[0-9]") -f 60 -keyint 1.0 -a "default_output|default_input" -c mp4 -bm cbr -cr full -q 26000 -k h264 -o "$video_path Recording $timestamp.mp4"
+gpu-screen-recorder -w screen -f 60 -keyint 1.0 -a "default_output|default_input" -s 1920x1080 -c mp4 -bm cbr -cr full -q 26000 -k h264 -o "$video_path Recording $timestamp.mp4"
--- .local/bin/start-replay.sh
+++ .local/bin/start-replay.sh
@@ -3,4 +3,4 @@
 video_path="$HOME/Videos/Clips"
 mkdir -p "$video_path"
 notify-send -t 500 -u low -- "GPU Screen Recorder" "Started recording"
-gpu-screen-recorder -w $(xrandr | grep -o "DP-[0-9]") -f 60 -keyint 1.0 -a "default_output|default_input" -c mp4 -bm cbr -cr full -q 26000 -k h264 -r 30 -o "$video_path"
+gpu-screen-recorder -w screen -f 60 -keyint 1.0 -a "default_output|default_input" -s 1920x1080 -c mp4 -bm cbr -cr full -q 26000 -k h264 -r 30 -o "$video_path"
