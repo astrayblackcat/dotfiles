b5c93bf410a5ac2e35d71f6fba8ab68fa1fd721c
Blake <itsmazzyy@gmail.com>
2025-05-18 15:52:39 +0100

    update scripts, add some nvim config files

--- .local/bin/start-record.sh
+++ .local/bin/start-record.sh
@@ -5,5 +5,5 @@ video_path="$HOME/Videos/Recordings/"
 echo $timestamp
 mkdir -p "$video_path"
 notify-send -t 500 -u low -- "GPU Screen Recorder" "Started recording"
-gpu-screen-recorder -w screen -f 60 -keyint 1.0 -a "default_output|default_input"\
-                    -s 2560x1440 -c mp4 -bm cbr -cr full -q 26000 -k h264 -o "$video_path Recording $timestamp.mp4"
+gpu-screen-recorder -w DP-3 -f 60 -keyint 1.0 -a "default_output|default_input"\
+                    -s 1920x1080 -c mp4 -bm cbr -cr full -q 26000 -k h264 -o "$video_path Recording $timestamp.mp4"
--- .local/bin/start-replay.sh
+++ .local/bin/start-replay.sh
@@ -3,5 +3,5 @@
 video_path="$HOME/Videos/Clips"
 mkdir -p "$video_path"
 notify-send -t 500 -u low -- "GPU Screen Recorder" "Started recording"
-gpu-screen-recorder -w screen -f 60 -keyint 1.0 -a "default_output|default_input"\
+gpu-screen-recorder -w DP-3 -f 60 -keyint 1.0 -a "default_output|default_input"\
                     -s 1920x1080 -c mp4 -bm cbr -cr full -q 26000 -k h264 -r 30 -o "$video_path"
