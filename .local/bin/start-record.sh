e142ea0b4e6a2f26f4ac300a1de9b785d2b946e6
Blake <itsmazzyy@gmail.com>
2025-07-24 14:28:51 +0100

    dropping bitrate on GPU screen recorder because discord file size limits

--- .local/bin/start-record.sh
+++ .local/bin/start-record.sh
@@ -6,4 +6,4 @@ echo $timestamp
 mkdir -p "$video_path"
 notify-send -t 500 -u low -- "GPU Screen Recorder" "Started recording"
 gpu-screen-recorder -w DP-3 -f 60 -keyint 1.0 -a "default_output|default_input"\
-                    -s 1920x1080 -c mp4 -bm cbr -cr full -q 26000 -k h264 -o "$video_path Recording $timestamp.mp4"
+                    -s 1920x1080 -c mp4 -bm cbr -cr full -q 13000 -k h264 -o "$video_path Recording $timestamp.mp4"
--- .local/bin/start-replay.sh
+++ .local/bin/start-replay.sh
@@ -4,4 +4,4 @@ video_path="$HOME/Videos/Clips"
 mkdir -p "$video_path"
 notify-send -t 500 -u low -- "GPU Screen Recorder" "Started recording"
 gpu-screen-recorder -w DP-2 -f 60 -keyint 1.0 -a "default_output|default_input"\
-                    -s 1920x1080 -c mp4 -bm cbr -cr full -q 26000 -k h264 -r 30 -o "$video_path"
+                    -s 1920x1080 -c mp4 -bm cbr -cr full -q 13000 -k h264 -r 30 -o "$video_path"
