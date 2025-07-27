6e04cbd009e3d47b421fb6cacee16bfbb6d6832d
Blake <itsmazzyy@gmail.com>
2025-07-27 17:53:38 +0100

    remove superfluous comments from script

--- .local/bin/get-proton-ge.sh
+++ .local/bin/get-proton-ge.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-# make temp working directory
 echo "Creating temporary working directory..."
 tmp_dir=/tmp/proton-ge-custom
 rm -rf "$tmp_dir"
 mkdir "$tmp_dir"
 cd "$tmp_dir"
 
-# download tarball
 echo "Fetching tarball URL..."
 tarball_url=$(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep browser_download_url | cut -d\" -f4 | grep .tar.gz)
 tarball_name=$(basename "$tarball_url")
@@ -16,7 +14,6 @@ tarball_name=$(basename "$tarball_url")
 echo "Downloading tarball: $tarball_name..."
 curl -# -L "$tarball_url" -o "$tarball_name"
 
-# download checksum
 echo "Fetching checksum URL..."
 checksum_url=$(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep browser_download_url | cut -d\" -f4 | grep .sha512sum)
 checksum_name=$(basename "$checksum_url")
@@ -24,18 +21,15 @@ checksum_name=$(basename "$checksum_url")
 echo "Downloading checksum: $checksum_name..."
 curl -# -L "$checksum_url" -o "$checksum_name" --no-progress-meter
 
-# check tarball with checksum
 echo "Verifying tarball $tarball_name with checksum $checksum_name..."
 sha512sum -c "$checksum_name"
 
 # if result is ok, continue
 
-# make steam directory if it does not exist
 echo "Creating Steam directory if it does not exist..."
 proton_dir=~/.steam/root/compatibilitytools.d/Proton-GE
 mkdir -p "$proton_dir"
 
-# extract proton tarball to steam directory
 echo "Extracting $tarball_name to Steam directory..."
 rm -rf "$proton_dir"/*
 tar -xf "$tarball_name" -C ~/.steam/root/compatibilitytools.d/Proton-GE --strip-components=1
